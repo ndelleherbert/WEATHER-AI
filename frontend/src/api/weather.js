91b4d9ebff3a7b7f3bdf0d3e12c794cbb37cbb0d
Ndelle Njumbe Herbert <ndelleh04@gmail.com>
2026-06-05 12:06:49 +0100

    force rebuild

--- frontend/src/api/weather.js
+++ frontend/src/api/weather.js
@@ -1,13 +1,28 @@
-import axios from "axios";
+/*import axios from "axios";
 
 // 🌍 Auto switch between local and production
 const BASE_URL =
   import.meta.env.MODE === "development"
+
     ? "http://localhost:8000"
     : import.meta.env.VITE_API_URL;
 
 const API = axios.create({
   baseURL: BASE_URL,
+});
+
+export const analyzeWeather = async (payload) => {
+  const res = await API.post("/weather/analysis", payload);
+  return res.data;
+};
+*/
+
+
+
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const analyzeWeather = async (payload) => {
